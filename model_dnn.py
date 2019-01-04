296ed6c351454befa9b4dd409b01b86708d01637
Pawel_Czajka <paw787878@gmail.com>
2019-01-04 16:50:59 +0100

    dodalem komentarze oraz dropout

--- model_dnn.py
+++ model_dnn.py
@@ -1,9 +1,26 @@
+#Model ktory mozna trenowac na danych przygotowanych przez klase
+# zapisywacz. Korzysta z klasy tf.estimator.DNNClassifier . 
+# czyli glebokiej sieci neuronowej. 
+# Staram sie korzystac z tutorialu 
+# https://www.tensorflow.org/guide/performance/datasets
+# to znaczy stosuje prefetch ze strony
+# https://www.tensorflow.org/api_docs/python/tf/data/Dataset#prefetch
+# ktore przygotowuje nowe dane gdy model dalej obrabia poprzednie dane
+# jest mozliwosc przygotowania danych na kilku thredach (argument
+# ile_threadow w funkcji train)
+# oraz daje mozliwosc zrobienia cache dla datasetu. 
+# to znaczy argument czy_cache w train. Jesli cachujemy to 
+# dane sa zapisane w pierwszej epoce trenowania na ramie
+# i pozniej szybciej do nich dotrzec w kolejnych epokach.
+# Jesli dataset miesci sie na ramie to warto dac True.
+
 import tensorflow as tf
 import numpy as np
 import json
 import os
 import zapisywacz
 
+#ogolne funkcje
 def zapisz_json(co,gdzie):
     f=open(gdzie,'w')
     f.write(json.dumps(co))
@@ -12,15 +29,15 @@ def wczytaj_json(skad):
     f=open(skad,'r')
     return json.loads(f.read())
 
-def wyrwij_label(slownik):
-    l= slownik.pop('l')
-    return slownik, l
-
-#po to, by nigdy nie pomieszaly sie nazwy kluczy zapisywanego w binarnym
-#pliku slownika
+# Zmienia nazwe kategorycznych feature tak, by
+# nie pomieszaly sie z wykorzystywanymi juz kluczami
+# w slowniku
 def powieksz_nazwe_kategorycznego(nazwa):
     return 'kat_fet_' + nazwa
 
+#tworzy feature columns dla naszego modelu,
+# zalaczam dokumentacje, jesli ktos chce cos zmienic
+# https://www.tensorflow.org/guide/feature_columns
 def rob_feature_columns(nazwa_zrodla):
     kategoryczne_typy = zapisywacz.wczytaj_typy_kategorycznych(nazwa_zrodla)
     numeryczne_typy = zapisywacz.wczytaj_typy_numerycznych(nazwa_zrodla)
@@ -34,10 +51,26 @@ def rob_feature_columns(nazwa_zrodla):
             vocabulary_list = kat[1],
             dtype = tf.int32
         )
+        #tutaj zamiast indicator column, ktora robi one-hot encoding
+        #mozna zrobic 
+        #https://www.tensorflow.org/api_docs/python/tf/feature_column/embedding_column
+        #ktora uczy sie embeddingu kategorycznych labelkow w jakims
+        #R^n. To jest dobrym pomyslem, gdy jest sporo mozliwych
+        # wartosci tego kategorycznego feature. 
         hot = tf.feature_column.indicator_column(nowy)
         wyn.append(hot)
     return wyn
 
+#Dosc uniwersalna funkcja przygotowujaca dane dla modelu. 
+# zrodlo_danych to folder przygotowany przez zapisywacz.py
+# folder_modelu to folder utworzony funkcja tworz_folder (folder modelu)
+# batch_size to batch size
+# czy_shuffle to czy tasowac dane przed wrzuceniem do modelu
+# ile_threadow to na ilu threadach przygotowywac dane
+# czy_repeat to czy brac dane z poczatku gdy skoncza sie z konca
+# buffer_size liczy sie gdy czy_shuffle = True, 
+#   jak duzy ma byc bufer do tasowania
+# czy_cache to czy wczytac dane na ram w celu szybszego ich czyania
 def input_fn(zrodlo_danych, folder_modelu, batch_size,  
          czy_shuffle, ile_threadow, czy_repeat, buffer_size,
          czy_cache ):
@@ -55,6 +88,9 @@ def input_fn(zrodlo_danych, folder_modelu, batch_size,
             for i in range(len(kategoryczne)):
                 slownik[ powieksz_nazwe_kategorycznego(kategoryczne[i][0])] = kat_tab[:, i]
             return slownik
+        def wyrwij_label(slownik):
+            l= slownik.pop('l')
+            return slownik, l
         dataset = dataset.map(odczep_kategoryczne, num_parallel_calls = ile_threadow)
         dataset = dataset.map(normalizacja,num_parallel_calls = ile_threadow)
         dataset = dataset.map(wyrwij_label,num_parallel_calls = ile_threadow)
@@ -67,13 +103,22 @@ def input_fn(zrodlo_danych, folder_modelu, batch_size,
     return wynikowa
 
 #nazwa_folderu to jak sie bedzie nazywac folder modelu
-#glowne_zrodlo_danych to plik z ktorego beda pochodzic dane
-# do policzenia danych do normalizacji danych
-def tworz_folder(nazwa_folderu, glowne_zrodlo_danych, hidden_units, na_ilu_statystyki = 10000):
+#glowne_zrodlo_danych to plik z ktorego beda pochodzic dane.
+# (uzywam do policzenia statystyk by pozniej moc liczyc normalizacje)
+#hidden units to jaka ma byc architektura modelu (np [30, 5] to znaczy, ze
+# jest jedna ukryta warstwa co ma 30 neuronow, po niej taka co ma 5, 
+# potem jest ta wynikowa warstwa)
+# dropout to liczba od 0. do 1., im wieksza tym wieksa
+# czesc neuronow jest ignorowana przy uczeniu. zwieksza to 
+# regularyzacje modelu. Jesli None to nie ma dropoutu.
+# zwraca utworzony model. Mozna ten zwracany model zignorowac.
+def tworz_folder(nazwa_folderu, glowne_zrodlo_danych, hidden_units, 
+    dropout, na_ilu_statystyki = 10000):
     assert not os.path.isdir(nazwa_folderu)
     os.mkdir(nazwa_folderu)
     zapisz_json(glowne_zrodlo_danych, nazwa_folderu + '/glowne_zrodlo_danych')
     zapisz_json(hidden_units, nazwa_folderu + '/hidden_units')
+    zapisz_json(dropout, nazwa_folderu + '/dropout')
     def licz_statystyki(nazwa_zrodla, na_ilu):
         dataset = zapisywacz.parsuj_i_batchuj(nazwa_zrodla, na_ilu, 1)
         iterator = dataset.make_one_shot_iterator()
@@ -98,36 +143,47 @@ def tworz_folder(nazwa_folderu, glowne_zrodlo_danych, hidden_units, na_ilu_staty
         hidden_units = hidden_units,
         feature_columns = feature_columns,
         model_dir = nazwa_folderu + '/model',
-        n_classes = 2
+        n_classes = 2,
+        dropout = dropout
     )
-    #trzeba cos potrenowac by 
+    #trzeba cos potrenowac by powstaly losowe poczatkowe wagi 
+    # modelu
     model.train(
         input_fn = input_fn(glowne_zrodlo_danych, nazwa_folderu,1,False, 1,False, 1, False),
         steps = 1
     )
     return model
     
-
+#pozwala odczytac zapisane statyki danych
 def odczytaj_mean_var(nazwa_folderu):
     mean = np.loadtxt(nazwa_folderu + '/mean')
     var = np.loadtxt(nazwa_folderu + '/var')
     return mean, var
 
-#odczytujemy juz istniejacy model
+#odczytujemy juz istniejacy model 
 def wczytaj_model(nazwa_folderu):
     assert os.path.isdir(nazwa_folderu + '/model')
     glowne_zrodlo_danych = wczytaj_json(nazwa_folderu + '/glowne_zrodlo_danych')
     feature_columns = rob_feature_columns(glowne_zrodlo_danych)
     hidden_units = wczytaj_json(nazwa_folderu + '/hidden_units')
+    dropout = wczytaj_json(nazwa_folderu + '/dropout')
     return tf.estimator.DNNClassifier(
         hidden_units = hidden_units,
         feature_columns = feature_columns,
         model_dir = nazwa_folderu + '/model',
-        n_classes = 2
+        n_classes = 2,
+        dropout = dropout
     )
 
-
-def train(zrodlo_danych, folder_modelu, steps, czy_cache, batch_size = 128,  
+#trenuje model. 
+# zrodo_danych to folder zapisany przy pomocy zapisywacz.py
+# folder_modelu to folder_modelu 
+# steps to ile batches trzeba wczytac
+# czy_cache to czy wczytac dane do ram. jesli dataset
+#        miesci si ena ramie to warto to zrobic.
+# 
+# reszta argumentow opisana przy okazji input_fn. 
+def train(zrodlo_danych, folder_modelu, steps, czy_cache, batch_size = 128,
          czy_shuffle = True, ile_threadow = 1, czy_repeat = True, buffer_size = 1000
          ):
     model = wczytaj_model(folder_modelu)
@@ -138,6 +194,10 @@ def train(zrodlo_danych, folder_modelu, steps, czy_cache, batch_size = 128,
         steps = steps
     )
 
+# ewaluuje model
+# zrodo_danych to folder zapisany przy pomocy zapisywacz.py
+# folder_modelu to folder_modelu
+# reszta opisana przy okazji input_fn 
 def evaluate(zrodlo_danych, folder_modelu, steps, batch_size = 128,  
         ile_threadow = 1):
     model = wczytaj_model(folder_modelu)
--- zapisywacz.py
+++ zapisywacz.py
@@ -1,7 +1,49 @@
+#pozwala tworzyc datasety danych tensorflowowych
+#ktore sa kompatybilne z klasa model_dnn.py
+
+#zapisuje dane w formacie TFRecords file. Jest on 
+#wspomniany w https://www.tensorflow.org/api_guides/python/reading_data 
+#pod 'standard tensorflow format', podobno estymator jest zoptymalizowany
+#do czytania takiego czegos. 
+#, oraz lepiej opisany w https://www.tensorflow.org/tutorials/load_data/tf-records 
+# oraz jeszcze lepiej opisane tu
+#  https://medium.com/mostly-ai/tensorflow-records-what-they-are-and-how-to-use-them-c46bc4bbb564
+# 
+# Bardzo wysokopoziomowo format ten pozwala na zapisanie slownikow gdzie
+# klucze - stringi to nazwy featurow ( w tym formacie label jest
+# traktowany dokladnie tak samo jak feture ), zas wartosci to 
+# listy floatow, listy intow lub czystych bitow ( jesli ktos chce zapisac obrazki to chyba
+# uzyteczne).
+# 
+# w tym przypadku klucze zapisanego slownika to 
+# - 'l' czyli label, 0 oznacza tlo, zas 1 oznacza sygnal
+# - 'n' czyli numeryczne wartosci. Czyli to jest lista
+#       wszystkich floatow charakteryzujacych przypadek
+#       Pakuje wszystkie te floatowe feature do jednego
+#       worka gdyz wowczas powstale pliki binarne sa
+#       mniejsze i szybciej sie je czyta i przetwarza 
+#       przy trenowaniu.
+# - 'c' czyli kategoryczne wartosci. To jest lista intow, gdzie
+#        kazdy int to jakas kategoryczna cecha np ladunek czegos. 
+#       znowu wrzucone do jednego worka z powodu checi 
+#       oszczedzania miejsca i przyspieszenia dzialania trenowania
+#
+# W folderze z datasetem znajduja sie takze zapisane jsonem listy:
+# - W pliku 'n' jest 
+#   Lista z zapisanymi nazwami numerycznych featurow. Elementy listy to
+#   krotki (technicznie tez listy) (nazwa_feature, index_pocz, index_konc) gdzie
+#   index_pocz to index od ktorego zaczynaja sie flaoty tego feature w liscie
+#   skonkatenowanych numerycznch feature (czyli pod kluczem 'n' w zapisanym
+#   TFRecord pliku)
+# - w pliku 'c' jest analogiczny opis kategorycznych danych, to znaczy
+#    jest zapisana jsonem lista gdzie elementy to 
+#   krotki (nazwa_feature, lista_mozliwych_wartosci_tego_feature)
 import tensorflow as tf
 import numpy as np
 import json
 import os
+
+
 
 #na poczatek ogolne funkcje
 
@@ -28,19 +70,33 @@ def _int64_feature(values):
   """Returns an int64_list from a bool / enum / int / uint."""
   return tf.train.Feature(int64_list=tf.train.Int64List(value=values))
 
+#odczytuje nazwy numerycznych feature dla danych z folderu 'nazwa_folderu'
 def wczytaj_typy_numerycznych(nazwa_folderu):
     return wczytaj_json(nazwa_folderu + '/n')
+
+#odczytuje nazwy kategorycznych feature dla danych z folderu 'nazwa_folderu'
 def wczytaj_typy_kategorycznych(nazwa_folderu):
     return wczytaj_json(nazwa_folderu + '/c')
 
-#nazwa tworzonego folderu, slownik typu nazwa -> rozmiar, c to typu nazwa -> lista_wartosci
-# zwraca typy numeryczych danych, typy kategorycznych danych, oraz
-#writer ktorym mozna pisac do pliku (nadpisywac chyba)
-#te zwracane uzywa sie w zapisz_jeden_przyklad
+#Tworzy pusty folder-dataset. Argumenty to 
+#nazwa tworzonego folderu, slownik typu nazwa_feature -> rozmiar_tego_feature
+#  (nalezy podac numeryczne feature oraz ich rozmiar, np
+#  czteroped pewnie ma rozmiar 4, a masa czegos pewnie 1)  ,
+#  c to typu nazwa_feature -> lista_wartosci (
+#   czyli jakie beda kategoryczne feature wraz z ich wartosciami).
+# oprocz tego, ze tworzy folder to daje 3 rzeczy, ktore sa potrzebne
+# w funkcji 'zapisz_jeden_przyklad' to znaczy
+# zwraca typy numeryczych feature (czyli wnetrze
+# pliku 'n'), typy kategorycznych feature (wnetrze
+# pliku 'c'), oraz
+# writer ktorym mozna pisac do pliku (nadpisywac chyba)
+# gdy skonczymy korzystac z writera warto go zamknac
+# komenda writer.close()
 def tworz_folder_do_zapisu(nazwa_folderu, n, c):
     assert not os.path.isdir(nazwa_folderu)
     os.mkdir(nazwa_folderu)
 
+    #tworzy liste numerycznych typow
     lista_numerycznych_typow = []
     rozmiar_dotychczasowego = 0
     for k in n.keys():
@@ -48,43 +104,59 @@ def tworz_folder_do_zapisu(nazwa_folderu, n, c):
         rozmiar_dotychczasowego += n[k]
     zapisz_json(lista_numerycznych_typow, nazwa_folderu + "/n")
     
+    #tworzy liste kategorycznych typow
     lista_kategorycznch_typow = []
     for k in c.keys():
         lista_kategorycznch_typow.append((k, c[k]))
     zapisz_json(lista_kategorycznch_typow, nazwa_folderu + "/c")
     writer = tf.python_io.TFRecordWriter(nazwa_folderu + '/dane')
+
     return lista_numerycznych_typow, lista_kategorycznch_typow, writer
 
-# n to slownik nazwa -> lista floatow lub (byc moze)
-#  numpy tablica shapu (k,) tez floatow, c to nazwa -> wartosc (typu int), l to 0 lub 1
-#pamietaj by po wszystkim zrobic writer.close() (to znaczy po zapisaniu ostatniego)
+#  n to slownik nazwa_numerycznego_feature -> jego_wartosc.
+#  jego_wartosc to tablica numpy o shapie (-1,) (czyli 1d) lub float lub
+#  lista floatow, c to nazwa_feature_kategorycznego -> wartosc_jego
+#  (typu int), l to 0 lub 1
+#  lnt, lkt, writer to trojka rzeczy zwracanych przez funkcje
+#  tworz_folder_do_zapisu
+#  pamietaj by po zapisaniu danych
+#  zrobic writer.close() (to znaczy po zapisaniu ostatniego)
 def zapisz_jeden_przyklad(n, c, l, lnt, lkt, writer):
+    #tworze slownik danych, ktory zostanie zapisany
+    # w binarnym pliku TFRecord
     slownik_danych = {}
+    #zapisuje label
     slownik_danych['l'] = _int64_feature([l])
-
-    def poprawiacz_numerycznego(lista):
-        return list(np.array(lista).reshape((-1,)))
+    #funkcja robiaca numpy tablice 1d z swojego argumentu
+    def poprawiacz_numerycznego(liczba_lub_lista_lub_numpy):
+        return list(np.array(liczba_lub_lista_lub_numpy).reshape((-1,)))
     
+    #konkatenuje numeryczne feature oraz wrzuca wynik do slonika danych
     lista_numerycznych = []
     for i in range(len(lnt)):
         rozwazany = lnt[i]
         lista_numerycznych += poprawiacz_numerycznego( n[ rozwazany[0] ] )
     slownik_danych['n']= _float_feature(lista_numerycznych)
 
+    #konkatenuje kategoryczne dane i wrzuca do slownika danych
     lista_kategorycznch= []
     for i in range(len(lkt)):
         rozwazany = lkt[i]
         lista_kategorycznch += [c[rozwazany[0]]]
     slownik_danych['c']= _int64_feature(lista_kategorycznch)
     
+    #serializuj slownik_danych i zapisz go 
     tr_ex = tf.train.Example(features= tf.train.Features(
         feature = slownik_danych))
     writer.write(tr_ex.SerializeToString())
 
-#tworzy slownik typo wpotrzebnych do parsowania
+#tworzy slownik typo wpotrzebnych do odczytania przykladu
 def tworz_slownik_typow_potrzebny_do_parsowania(nazwa_folderu):
     lista_numerycznych = wczytaj_typy_numerycznych(nazwa_folderu)
+    
+    #laczna liczbe numerycznych feature
     laczna_liczba = lista_numerycznych[len(lista_numerycznych) - 1][2]
+    
     lista_kategorycznych = wczytaj_typy_kategorycznych(nazwa_folderu)
 
     read_features= {}
@@ -98,7 +170,10 @@ def odczytaj_dataset(nazwa_folderu):
     dataset = tf.data.TFRecordDataset(nazwa_folderu + '/dane')
     return dataset
 
-#parsuje kazdy przyklad z osobna. jest wiec wolna metoda
+#zwraca odczytany dataset ( to znaczy odczytuje
+# te zapisane slowniki i tworzy dataset z nich
+# ). kazdy przyklad odczytany odzielnie
+# wiec jest to dosc wolna metoda
 def parsuj_po_jednym(nazwa_folderu):
     read_features = tworz_slownik_typow_potrzebny_do_parsowania(nazwa_folderu)
     def parsuj(serialized_example):
@@ -108,8 +183,9 @@ def parsuj_po_jednym(nazwa_folderu):
     dataset = dataset.map(parsuj)
     return dataset
 
-#od razu batchuje przy parsowaniu, wiec szybsza
-#podajemy ile threadow ma robic ten preprocesing by bylo szybciej
+#podobna do parsuj_po_jednym, ale szybsza. Odczytuje
+# od razu wiele przykladow i zwraca batch. argument ile_threadow
+# oznacza na ilu threadach ma zachodzic ten proces parsowania
 def parsuj_i_batchuj(nazwa_folderu, batch_size, ile_threadow):
     read_features = tworz_slownik_typow_potrzebny_do_parsowania(nazwa_folderu)
     def parsuj(serialized_examples):
